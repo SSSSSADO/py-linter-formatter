2094137ef40b65931608366b56835d6ca2c8b61a
SSSSSADO <grushkovik.danil@gmail.com>
2025-11-10 20:13:16 +0300

    test: add activity for today

--- app/main.py
+++ app/main.py
@@ -11,6 +11,7 @@ def format_linter_error(error: dict) -> dict:
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {"errors": [format_linter_error(error)for error in errors], "path": file_path, "status": "failed" if len(errors) != 0 else "passed"}
 
+# Test commit
 
 def format_linter_report(linter_report: dict) -> list:
     return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
